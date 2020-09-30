c1014b2c3a812e44f299ea28c7424f47fe26ebec
Anastasiya <schwarzer300708held@gmail.com>
2020-09-30 10:50:13 +0300

    feat: add solution for typical-array-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 
 exports.min = function min (array) {
+  if (Array.isArray(array) && array) {
+		let result = Math.min(...array);
+		
+		if (isNaN(result) || !isFinite(result)) {
+			return 0;
+		}
+		return result;
+  }
   return 0;
 }
 
 exports.max = function max (array) {
+  if (Array.isArray(array) && array) {
+    let result = Math.max(...array);
+
+    if (isNaN(result) || !isFinite(result)) {
+      return 0;
+    }
+    return result;
+  }
   return 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+	if (Array.isArray(array) && array) {
+    let result = array.reduce((sum, current) => sum + current, 0) / array.length;
+	
+		if (isNaN(result) || !isFinite(result)) {
+			return 0;
+		}
+		return result;
+	}
+	return 0;
 }
